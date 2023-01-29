d82d20c63c6ac2d5839e8a696a92ef9178ddf493
kia <kiyanosh.s@gmail.com>
2023-01-29 13:52:53 +0330

    added search for user and cafe_restaurant useing scope and ILIKE

--- app/models/cafe_restaurant_t.rb
+++ app/models/cafe_restaurant_t.rb
@@ -1,4 +1,6 @@
 class CafeRestaurantT < ApplicationRecord
-    has_many :user_roles, dependent: :destroy
-    has_many :users, through: :user_roles
+  has_many :user_roles, dependent: :destroy
+  has_many :users, through: :user_roles
+
+	scope :search, ->(query) { where("name ILIKE ?","%#{query}%") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many :user_roles, dependent: :destroy
-    has_many :cafe_restaurant_ts, through: :user_roles
+  has_many :user_roles, dependent: :destroy
+  has_many :cafe_restaurant_ts, through: :user_roles
+	
+	scope :search, ->(patern) { where("name ILIKE ?","%#{patern}%") }
 end
