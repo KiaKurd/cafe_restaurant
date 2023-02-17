e1c1c8bf1f885d6064d093f934519ff15a3bab0f
armanmoradi <armanmoradi8@gmail.com>
2023-02-17 14:40:33 +0330

    create index and show method in user_role_controller

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,4 +1,12 @@
 class UserRolesController < ApplicationController
 
-    
+    def index
+        @user_roles = UserRole.all
+        render json: @user_roles.to_json
+    end
+
+    def show   
+        @user_role = UserRole.find(params[:id])
+        render json: @user_role.to_json
+    end
 end
