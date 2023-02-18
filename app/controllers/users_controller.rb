bd145c41731298e954f390fb101f642406ceecee
kia <kiyanosh.s@gmail.com>
2023-02-18 12:28:29 +0330

    add index and show for user controler and set the routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+	before_action :set_user
+
+	#get /users/1
+  def show 
+		rander json: @user
+  end
+
+	#get /users
+  def index 
+		users = User.all
+
+		rander json: users
+  end
+
+  private
+
+	# Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
