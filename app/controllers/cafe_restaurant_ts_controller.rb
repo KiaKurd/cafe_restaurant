451e927280afdd8e265fef99d51067a6916d289c
Shahryaar7 <shahryar71@gmail.com>
2023-02-18 23:58:59 +0330

    show, index and destroy methods added

--- app/controllers/cafe_restaurant_ts_controller.rb
+++ app/controllers/cafe_restaurant_ts_controller.rb
@@ -1,2 +1,22 @@
 class CafeRestaurantTsController < ApplicationController
+  before_action :find_cafe, only: %i(show destroy)
+
+  def index
+    @cafes = CafeRestaurantT.all
+    render json: @cafes.to_json
+  end
+
+  def show
+    render json: @cafe.to_json
+  end
+
+  def destroy
+    @cafe.destroy
+  end
+
+  private
+
+  def find_cafe
+    @cafe = CafeRestaurantT.find(params[:id])
+  end
 end
