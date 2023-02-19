0d0c5cd4a49eb619b2c5cfffe955b0e1b350f40b
armanmoradi <armanmoradi8@gmail.com>
2023-02-20 00:35:39 +0330

    Add destroy method in user_role_controller and edit route

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,12 +1,23 @@
 class UserRolesController < ApplicationController
-
+    before_action :find_user_role, only: [:show, :destroy]
+    
     def index
         @user_roles = UserRole.all
         render json: @user_roles.to_json
     end
 
     def show   
-        @user_role = UserRole.find(params[:id])
         render json: @user_role.to_json
+    end
+    
+    def destroy
+        @user_role.destroy
+    end
+
+
+    private
+
+    def find_user_role
+        @user_role = UserRole.find(params[:id])
     end
 end
