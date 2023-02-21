11767a843fc7813d233fb90c87a785a41030fce9
kia <kiyanosh.s@gmail.com>
2023-02-21 23:48:59 +0330

    fix for 403 error from rails server while requesting for CRUD

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module CafeRestaurant
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # fix for 403 error from server
+    config.hosts << "www.example.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
