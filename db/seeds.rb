d3a4a6d3895ec729102129b48f9b7860a4a5ee80
kia <kiyanosh.s@gmail.com>
2023-02-24 13:40:38 +0330

    tried to add seeds to seeds.rb but getting a syntax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+
+(1..20).each do |id| 
+  User.create!(nd(1..20)
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    age: Faker::Date.birthday(min_age: 18, max_age: 70),
+    address: Faker::Address.street,
+    tel: Faker::PhoneNumber.phone_number
+  )
+end
+
+(1..20).each do |id|
+  CafeRestaurantT.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    website: Faker::Internet.url,
+    description: Faker::Restaurant.description
+  )
+end
+
+(1..20).each do |id|
+  UserRole.create!(
+    user_id: rand(1..20),
+    cafe_restaurant_t_id: rand(1..20),
+    active: %i(false active).sample,
+    role_type: %W(admin chef waiter).sample
+  )
+end
