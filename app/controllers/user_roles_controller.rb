0b0a2c032149ac8f81e0eb2d534e6a24e7751af4
armanmoradi <armanmoradi8@gmail.com>
2023-02-24 23:28:56 +0330

    Add create method to user_role_controller

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -14,10 +14,28 @@ class UserRolesController < ApplicationController
         @user_role.destroy
     end
 
+    def create  
+        @user_role = UserRole.new(user_role_params)
+        if @user_role.save 
+          render jsonapi: @user_role
+        else
+          render jsonapi_errors: { detail: @user_role.errors.messages }, status: :not_acceptable
+        end
+    end
+
 
     private
 
     def find_user_role
-        @user_role = UserRole.find(params[:id])
+      @user_role = UserRole.find(params[:id])
+    end
+
+    def user_role_params
+      params.require(:user_role).permit(
+        :user_id,
+        :cafe_restaurant_t_id,
+        :role_type,
+        :active,
+        )
     end
 end
