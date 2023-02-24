eb35fed6aecdec5c2f5f9c65d70a0c9598790f33
armanmoradi <armanmoradi8@gmail.com>
2023-02-24 23:14:16 +0330

    Add active attribute to serializers

--- app/serializers/serializable_user_role.rb
+++ app/serializers/serializable_user_role.rb
@@ -1,7 +1,7 @@
 class SerializableUserRole < JSONAPI::Serializable::Resource
   type 'user_role'
 
-  attributes :role_type
+  attributes :role_type, :active
 
   belongs_to :user
   belongs_to :cafe_restaurant_t
