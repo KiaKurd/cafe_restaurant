a4ee6ef8c8d1662f9e1eec10a23c5f1487469a37
kia <kiyanosh.s@gmail.com>
2023-02-24 13:57:48 +0330

    fixed user serializer

--- app/serializable/serializable_user.rb
+++ app/serializable/serializable_user.rb
@@ -1,7 +1,7 @@
 class SerializableUser < JSONAPI::Serializable::Resource
   type 'user'
 
-  attribute :name, :email, :age, :address, :tel
+  attributes :name, :email, :age, :address, :tel
   
   has_many :user_roles
   has_many :cafe_restaurant_ts
