c04a7d675eebfe34eb491df3f6938deae28e872e
kia <kiyanosh.s@gmail.com>
2023-02-24 13:52:45 +0330

    edit user serilizer

--- app/serializable/serializable_user.rb
+++ app/serializable/serializable_user.rb
@@ -1,12 +1,8 @@
 class SerializableUser < JSONAPI::Serializable::Resource
-  type 'users'
-  attribute :name
-  attribute :address
-  attribute :tel
-  attribute :created_at
-  attribute :updated_at
-  attribute :email
-  attribute :age
+  type 'user'
+
+  attribute :name, :email, :age, :address, :tel
+  
   has_many :user_roles
   has_many :cafe_restaurant_ts
 end
