5b507929dba39e7574f2822655e3108bec20bcfe
Shahryaar7 <shahryar71@gmail.com>
2023-02-24 23:56:37 +0330

    serializer for cafe created

---
+++ app/serializers/serializable_cafe_restaurant_t.rb
@@ -0,0 +1,8 @@
+class SerializableCafeRestaurantT < JSONAPI::Serializable::Resource
+  type 'cafe_restaurant_t'
+
+  attributes :name, :website, :address, :description, :employees
+
+  has_many :user_roles
+  has_many :users, through: :user_roles
+end
