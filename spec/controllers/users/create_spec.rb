0990c297c97bd37434b753bba90f85b7a43544c1
kia <kiyanosh.s@gmail.com>
2023-02-26 15:12:57 +0330

    fix update tests now all tests passes

--- spec/controllers/users/create_spec.rb
+++ spec/controllers/users/create_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe UsersController, type: :request do
   describe '#create' do
     context 'valid request' do
       it 'creates a new user' do 
-        user = create(:user)
         attributes  = FactoryBot.attributes_for(:user)
 
         post "/users", params: {
@@ -13,22 +12,49 @@ RSpec.describe UsersController, type: :request do
 
         expect(response).to have_http_status(:created) #201
 
-        json = JSON.parse(response.body)
+        json = parse_json
         expect(json['data']['attributes']['email']).to eql(attributes[:email])
       end
     end
     context 'invalid request' do 
-      it 'blank attributes' do
-        # binding.pry
-        user = create(:user)
+      it 'blank attribute name' do
         attributes = FactoryBot.attributes_for(:user, name: nil)
 
         post "/users", params: {
           user: attributes
         }
 
-        expect(response.status).to eql(406)
+        expect(response.status).to eql(400)
+        expect(parse_json).to eql({"name"=>["can't be blank"]})
+      end
+
+      it 'black attribute email' do
+        attributes = FactoryBot.attributes_for(:user, email: nil)
+        
+        post "/users", params:{
+          user: attributes
+        }
+
+        expect(response.status).to eql(400)
+        expect(parse_json).to eql({"email"=>["can't be blank", "Invalid format"]})
+      end
+
+      it 'when age is less then 18' do 
+        attributes = FactoryBot.attributes_for(:user, age: 17.years.ago)
+
+        post "/users", params: {
+          user: attributes
+        }
+
+        expect(response.status).to eql(400)
+        expect(parse_json).to eql({"age"=>["must be at least 18 yesrs old"]})
       end
     end
+  end
+
+  private 
+
+  def parse_json
+    JSON.parse(response.body)
   end
 end
