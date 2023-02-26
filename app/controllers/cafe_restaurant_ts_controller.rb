df447add0736f81ec98ae15669193773f8909f2d
Shahryaar7 <shahryar71@gmail.com>
2023-02-26 11:32:53 +0330

    create and update added

--- app/controllers/cafe_restaurant_ts_controller.rb
+++ app/controllers/cafe_restaurant_ts_controller.rb
@@ -1,5 +1,5 @@
 class CafeRestaurantTsController < ApplicationController
-  before_action :find_cafe, only: %i(show destroy)
+  before_action :find_cafe, only: %i(show destroy update)
 
   def index
     @cafes = CafeRestaurantT.all
@@ -14,9 +14,36 @@ class CafeRestaurantTsController < ApplicationController
     @cafe.destroy
   end
 
+  def create
+    @cafe = CafeRestaurantT.new(cafe_params)
+    if @cafe.save
+      render jsonapi: @cafe
+    else
+      render json: @cafe.errors, status: :not_acceptable
+    end
+  end
+
+  def update
+    if @cafe.update(cafe_params)
+      render jsonapi: @cafe
+    else
+      render jsonapi: @cafe.errors
+    end
+  end
+
   private
 
   def find_cafe
     @cafe = CafeRestaurantT.find(params[:id])
+  end
+
+  def cafe_params
+    params.require(:cafe_restaurant_t).permit(
+      :name,
+      :website,
+      :address,
+      :description,
+      :employees
+      )
   end
 end
