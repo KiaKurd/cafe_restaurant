988a6bf6fff3f9c94c5396dcc4460859caeece6b
Shahryaar7 <shahryar71@gmail.com>
2023-02-26 11:31:46 +0330

    routes are set

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cafe_restaurant_ts, only: [:index, :show, :destroy]
+  resources :cafe_restaurant_ts
 end
