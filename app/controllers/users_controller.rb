63f8033561a5d0933c80869ae4624649f48ef336
kia <kiyanosh.s@gmail.com>
2023-02-26 20:02:57 +0330

    add serializer and roles for what attributes can be send from server

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,25 @@ class UsersController < ApplicationController
   def index
 		@users = User.all
 
-		render jsonapi: @users, exclude: %w(cafe_restaurant_ts user_roles)
+		render jsonapi: @users, 
+    include: %w(user_role cafe_restaurant_t),
+    fields: {
+      user: %w(name address age),
+      user_role: %w(role_type active),
+      cafe_restaurant_t: %w(name website description)
+    }
   end
 
 	#GET /users/id
   def show
-		render jsonapi: @user, exclude: %W(relationships)
+		render jsonapi: @user, fields: { user: %w(name address age) }
   end
   
   #POST /users
   def create
     user = User.new(user_params)
     if user.save
-      render jsonapi: user, status: :created
+      render jsonapi: user, exclude: %w(email), status: :created
     else
       render json: user.errors, status: :bad_request #400
     end
@@ -25,7 +31,7 @@ class UsersController < ApplicationController
 
   #PUT /users/id
   def update
-    if @user.update(user_params)
+    if user.update(user_params)
       render jsonapi: @user, status: :accepted #202
     else
       render json: @user.errors, status: :bad_request #400
