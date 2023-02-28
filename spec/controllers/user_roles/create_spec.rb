494f0b0ed6edfbe8c3fc3c658af8c94ba0c4bb8c
armanmoradi <armanmoradi8@gmail.com>
2023-03-01 01:32:51 +0330

    edit create_spec

--- spec/controllers/user_roles/create_spec.rb
+++ spec/controllers/user_roles/create_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe UserRolesController, type: :request do
     context 'valid requests' do
       it 'creates user_roles' do
         # Require data
-        user_role   = create(:user_role)
+        user_role = create(:user_role)
         user = create(:user)
         cafe_restaurant_t = create(:cafe_restaurant_t)
 
-        attributes  = FactoryBot.attributes_for(:user_role),
+        attributes  = FactoryBot.attributes_for(:user_role)
         attributes[:user_id] = user.id
         attributes[:cafe_restaurant_t_id] = cafe_restaurant_t.id
 
@@ -17,11 +17,11 @@ RSpec.describe UserRolesController, type: :request do
         post "/user_roles", params: {
           user_role: attributes,
         }
-        #  byebug
+        # byebug
         # Expects
-        expect(response.status).to eql(200)
+        expect(response.status).to eql(201)
         expect(JSON.parse(response.body)['data']['attributes']['role_type']).to eql(attributes[:role_type])
-        expect(JSON.parse(response.body)['data']['relationships']['user']['data']['id']).to eql(attributes[:user_id].to_s)
+        expect(JSON.parse(response.body)['data']['relationships']['user']['data']['id'].to_i).to eql(attributes[:user_id])
       end
 
       it 'blank attributes' do
