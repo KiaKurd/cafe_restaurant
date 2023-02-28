32a663062c18a3eed97e3cd01497efd03e88f361
kia <kiyanosh.s@gmail.com>
2023-02-28 16:27:46 +0330

    fixed the errors and now update and create is working without any errors

--- app/services/users/create_service.rb
+++ app/services/users/create_service.rb
@@ -1,18 +1,32 @@
 module Users
-  class CreateService <  ApplicationService
-      def initialize(name, email, address, tel, age)
-        @name = params[:name]
-        @email = params[:email]
-        @address = params[:address]
-        @tel = params[:tel]
-        @age = params[:age]
-      end
+  class CreateService
+    attr_reader :user, :params
+
+    def initialize(params)
+      @params = params
+    end
 
     def call
-      exist_user = User.find_by(email: @email)
-      errors.add('Users already exist') if exist_user.present?
+      create_user
+      user
+    end
 
-      User.create(name: @name, email:@email, address: @address, tel: @tel, age: @age)
+    def create_user
+      exist_user = User.find_by(email: @email)
+      errors.add(:exist_user, 'User is already exist') if exist_user.present?
+      
+      @user = User.new
+      user.name = params[:name] if params.key?(:name)
+      user.email = params[:email] if params.key?(:email)
+      user.address = params[:address] if params.key?(:address)
+      user.tel = params[:tel] if params.key?(:tel)
+      user.age = params[:age] if params.key?(:age)
+
+      if user.valid? == true
+        user.save!
+      else 
+        user.errors
+      end
     end
   end
 end
--- app/services/users/update_service.rb
+++ app/services/users/update_service.rb
@@ -1,20 +1,31 @@
-module User 
-  class UpdateService < ApplicationService
-    object :user, class: user
-
-    def initialize(name, email, address, tel, age)
-      @name = params[:name]
-      @email = params[:email]
-      @address = params[:address]
-      @tel = params[:tel]
-      @age = params[:age]
+module Users
+  class UpdateService
+    attr_reader :params, :exist_user
+    def initialize(params)
+     @params = params
     end
 
     def call
-      exist_user = User.find_by(email: @email)
-      errors.add(:user, 'user not found') unless exist_user.present?
+      update_user
+      
+      exist_user
+    end
 
-      exist_user.update(name: @name, email: @email, address: @address, tel: @tel, age: @age)
+    def update_user
+      @exist_user = User.find_by_id(params[:id])
+      errors.add(:exist_user, 'user not found') unless exist_user.present?
+
+      exist_user.name = params[:name] if params.key?(:name)
+      exist_user.email = params[:email] if params.key?(:email)
+      exist_user.address = params[:address] if params.key?(:address)
+      exist_user.tel = params[:tel] if params.key?(:tel)
+      exist_user.age = params[:age] if params.key?(:age)
+
+      if exist_user.valid?
+        exist_user.save!
+      else 
+        exist_user.errors
+      end
     end
   end
 end
