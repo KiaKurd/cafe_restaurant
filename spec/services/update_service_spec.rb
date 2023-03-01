d0a3d673cb6d76d28fc54087964a7231752db3fa
armanmoradi <armanmoradi8@gmail.com>
2023-03-02 00:33:08 +0330

    add update_service_spec to user_role

---
+++ spec/services/update_service_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserRoles::UpdateService do
+  context 'valid call' do
+    it 'Update role_type successfully' do
+      # Required data
+      user_role  = create(:user_role)
+
+      params  = {role_type: 'chef'}
+
+      # Services
+      service_result = described_class.new(user_role, params).call
+
+      # Expects
+      expect(user_role.role_type).to eql(params[:role_type])
+    end
+
+    it 'Update active successfully' do
+      # Required data
+      user_role  = create(:user_role)
+
+      params  = {active: true}
+
+      # Services
+      service_result = described_class.new(user_role, params).call
+
+      # Expects
+      expect(user_role.active).to eql(params[:active])
+    end
+  end
+end
