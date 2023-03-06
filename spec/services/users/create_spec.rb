790ec153a554a22ccabe84866d9aa1cffd7c5072
kia <kiyanosh.s@gmail.com>
2023-03-06 16:46:39 +0330

    update services create and update rspec to work with update and create service after adding active_interaction

--- spec/services/users/create_spec.rb
+++ spec/services/users/create_spec.rb
@@ -5,30 +5,29 @@ RSpec.describe Users::CreateService do
     context 'valid call' do
       it 'creates a new user' do 
         attributes = FactoryBot.attributes_for(:user)
-
-        service_result = described_class.new(attributes).call
+        service_result = described_class.run(params: attributes)
         
         expect(service_result.valid?).to be_truthy
-        expect(service_result.name).to eql(attributes[:name])
+        expect(service_result.result.name).to eql(attributes[:name])
       end
     end
     context 'invalid call' do
       it 'invalid attribute blank name' do
         attributes = FactoryBot.attributes_for(:user, name: '')
         
-        service_result = described_class.new(attributes).call
-         
+        service_result = described_class.run(params: attributes)
+
         expect(service_result.valid?).to be_falsey
-        expect(service_result.errors.messages[:name]).to eql(["can't be blank"])
+        expect(service_result.errors.messages[:base]).to eql(["Name can't be blank"])
       end
 
       it 'invalid attribute nil eamil' do
         attributes = FactoryBot.attributes_for(:user, email: nil)
 
-        service_result = described_class.new(attributes).call
+        service_result = described_class.run(params: attributes)
         
         expect(service_result.valid?).to be_falsey
-        expect(service_result.errors.messages[:email]).to eql(["can't be blank", "Invalid format"])
+        expect(service_result.errors.messages[:base]).to eql(["Email can't be blank", "Email Invalid format"])
       end
     end
   end
--- spec/services/users/update_spec.rb
+++ spec/services/users/update_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Users::UpdateService do
       it 'update the user' do
         user = create(:user)
         attributes = FactoryBot.attributes_for(:user)
-        
-        service_result = described_class.new(user, attributes).call
+
+        service_result = described_class.run(user: user, params: attributes)
         
         expect(service_result.valid?).to be_truthy
-        expect(service_result.name).to eql(attributes[:name])
+        expect(service_result.result.name).to eql(attributes[:name])
       end
     end
     context 'invalid call' do
@@ -18,21 +18,21 @@ RSpec.describe Users::UpdateService do
         user = create(:user)
         attributes = FactoryBot.attributes_for(:user, name:'')
 
-        service_result = described_class.new(user, attributes).call
+        service_result = described_class.run(user: user, params: attributes)
         
         expect(service_result.valid?).to be_falsey
-        expect(service_result.errors.messages[:name]).to eql(["can't be blank"])
+        expect(service_result.errors.messages[:base]).to eql(["Name can't be blank"])
       end
 
       it 'invalid attributes nil eamil' do
         user = create(:user)
         attributes = FactoryBot.attributes_for(:user, email: nil)
 
-        service_result = described_class.new(user, attributes).call
+        service_result = described_class.run(user: user, params: attributes)
 
         expect(service_result.valid?).to be_falsey
-        expect(service_result.errors.messages[:email][0]).to eql("can't be blank")
-        expect(service_result.errors.messages[:email][1]).to eql("Invalid format")
+        expect(service_result.errors.messages[:base][0]).to eql("Email can't be blank")
+        expect(service_result.errors.messages[:base][1]).to eql("Email Invalid format")
       end
     end
   end
