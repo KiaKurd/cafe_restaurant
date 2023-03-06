db711c923ccbbda4915d6e1dca7e55cfe6879010
armanmoradi <armanmoradi8@gmail.com>
2023-03-06 21:22:18 +0330

    Edit update_sevice and add user_role.save to update_service

--- app/services/user_roles/update_service.rb
+++ app/services/user_roles/update_service.rb
@@ -9,16 +9,19 @@ module  UserRoles
 
     def call
       user_role_update
-      @user_role
+      
+      user_role
     end
 
     private
 
     def user_role_update
-      @user_role.role_type = params[:role_type] if params.key?(:role_type) 
-      @user_role.active = params[:active] if params.key?(:active)
-      @user_role.user_id = params[:user_id] if params.key?(:user_id)
-      @user_role.cafe_restaurant_t_id = params[:cafe_restaurant_t_id] if params.key?(:cafe_restaurant_t_id)
+      user_role.role_type = params[:role_type] if params.key?(:role_type) 
+      user_role.active = params[:active] if params.key?(:active)
+      user_role.user_id = params[:user_id] if params.key?(:user_id)
+      user_role.cafe_restaurant_t_id = params[:cafe_restaurant_t_id] if params.key?(:cafe_restaurant_t_id)
+      
+      user_role.save
     end
   end
 end
